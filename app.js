3dc8b69bf11eba9fd0c54443f21cf423994452ef
Aditya DS <adithya18062000@gmail.com>
2022-01-14 21:22:34 +0530

    chore: use routers and body parsing middleware

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 import express from 'express';
 
+import UserInteractionRouter from './routes/UserInteractionRoutes.js';
+import ContentRouter from './routes/ContentRoutes.js';
+import UserRouter from './routes/UserRoutes.js';
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(UserInteractionRouter);
+app.use(ContentRouter);
+app.use(UserRouter);
 
 app.get('/', async (req, res) => {
     res.send('Hello world!');
