ad1156a290eee3b78f8c753f530289e431372d55
Aditya DS <adithya18062000@gmail.com>
2022-01-17 10:13:40 +0530

    refactor: move user interaction route handlers to middleware

--- middleware/UserInteraction.js
+++ middleware/UserInteraction.js
@@ -1,12 +1,122 @@
 import dotenv from 'dotenv';
 import totp from 'totp-generator';
 import fetch from 'node-fetch';
+import mongoose from 'mongoose';
+import { UserInteraction } from '../models/UserInteraction.js';
 
 dotenv.config({ path: 'routes/.env' });
 
 const { TOTP_KEY } = process.env;
 
 export default class UserInteractionMiddleware {
+    static async readReadsHandler(req, res) {
+        try {
+            const { contentId } = req.params;
+            const reads = await UserInteraction.findOne({ contentId }, { reads: 1 });
+            res.send({
+                status: 'success',
+                data: {
+                    contentId,
+                    reads: reads ?? 0,
+                },
+            });
+        } catch (err) {
+            console.log(err);
+            res.send({
+                status: 'error',
+                message: err.message,
+            });
+        }
+    }
+
+    static async updateReadsHandler(req, res) {
+        try {
+            const { contentId } = req.params;
+            const userId = mongoose.Types.ObjectId(req.query.userId);
+            const reads = await UserInteraction.findOneAndUpdate({ contentId }, {
+                $addToSet: { reads: userId },
+            }, { new: true, upsert: true });
+            res.send({
+                status: 'success',
+                data: reads,
+            });
+        } catch (err) {
+            console.log(err);
+            res.send({
+                status: 'error',
+                message: err.message,
+            });
+        }
+    }
+
+    static async readLikesHandler(req, res) {
+        try {
+            const { contentId } = req.params;
+            const likes = await UserInteraction.findOne({ contentId }, { likes: 1 });
+            res.send({
+                status: 'success',
+                data: {
+                    contentId,
+                    likes: likes ?? 0,
+                },
+            });
+        } catch (err) {
+            console.log(err);
+            res.send({
+                status: 'error',
+                message: err.message,
+            });
+        }
+    }
+
+    static async updateLikesHandler(req, res) {
+        try {
+            const { contentId } = req.params;
+            const { likeState } = req.query;
+            const userId = mongoose.Types.ObjectId(req.query.userId);
+            const updates = {
+                1: { $addToSet: { likes: userId } },
+                0: { $pull: { likes: userId } },
+            };
+            const likes = await UserInteraction.findOneAndUpdate({
+                contentId,
+            }, updates[likeState], {
+                new: true,
+                upsert: true,
+            });
+            res.send({
+                status: 'success',
+                data: likes,
+            });
+        } catch (err) {
+            console.log(err);
+            res.send({
+                status: 'error',
+                message: err.message,
+            });
+        }
+    }
+
+    static async readStatsHandler(req, res) {
+        try {
+            const stats = await UserInteraction.aggregate().match({}).project({ likes: { $size: '$likes' }, reads: { $size: '$reads' }, contentId: 1 }).exec();
+            const topMetric = stats.reduce((acc, { likes, reads, contentId }) => {
+                acc[contentId] = likes + reads;
+                return acc;
+            }, {});
+            res.send({
+                status: 'success',
+                data: topMetric,
+            });
+        } catch (err) {
+            console.log(err);
+            res.send({
+                status: 'error',
+                message: err.message,
+            });
+        }
+    }
+
     static checkTOTP(req, res, next) {
         const receivedTotp = req.headers['x-totp'];
         const totpTimestamp = req.headers['x-totp-generation-timestamp'];
--- routes/UserInteractionRoutes.js
+++ routes/UserInteractionRoutes.js
@@ -1,121 +1,19 @@
 import { Router } from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { UserInteraction } from '../models/UserInteraction.js';
 import UserInteractionMiddleware from '../middleware/UserInteraction.js';
 
 dotenv.config({ path: 'routes/.env' });
 
 const UserInteractionRouter = new Router();
+const {
+    existsContent, existsUser, checkTOTP,
+    readReadsHandler, updateReadsHandler, readLikesHandler, updateLikesHandler, readStatsHandler,
+} = UserInteractionMiddleware;
 
-UserInteractionRouter.get('/content/reads/:contentId', UserInteractionMiddleware.existsContent, async (req, res) => {
-    // retrieve read count
-    try {
-        const { contentId } = req.params;
-        const reads = await UserInteraction.findOne({ contentId }, { reads: 1 });
-        res.send({
-            status: 'success',
-            data: {
-                contentId,
-                reads: reads ?? 0,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserInteractionRouter.put('/content/reads/:contentId', UserInteractionMiddleware.existsUser, UserInteractionMiddleware.existsContent, async (req, res) => {
-    // update read count
-    try {
-        const { contentId } = req.params;
-        const userId = mongoose.Types.ObjectId(req.query.userId);
-        const reads = await UserInteraction.findOneAndUpdate({ contentId }, {
-            $addToSet: { reads: userId },
-        }, { new: true, upsert: true });
-        res.send({
-            status: 'success',
-            data: reads,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserInteractionRouter.get('/content/likes/:contentId', UserInteractionMiddleware.existsContent, async (req, res) => {
-    // retrieve like count
-    try {
-        const { contentId } = req.params;
-        const likes = await UserInteraction.findOne({ contentId }, { likes: 1 });
-        res.send({
-            status: 'success',
-            data: {
-                contentId,
-                likes: likes ?? 0,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserInteractionRouter.put('/content/likes/:contentId', UserInteractionMiddleware.existsUser, UserInteractionMiddleware.existsContent, async (req, res) => {
-    // update like count
-    try {
-        const { contentId } = req.params;
-        const { likeState } = req.query;
-        const userId = mongoose.Types.ObjectId(req.query.userId);
-        const updates = {
-            1: { $addToSet: { likes: userId } },
-            0: { $pull: { likes: userId } },
-        };
-        const likes = await UserInteraction.findOneAndUpdate({ contentId }, updates[likeState], {
-            new: true,
-            upsert: true,
-        });
-        res.send({
-            status: 'success',
-            data: likes,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserInteractionRouter.get('/content/stats', UserInteractionMiddleware.checkTOTP, async (req, res) => {
-    try {
-        const stats = await UserInteraction.aggregate().match({}).project({ likes: { $size: '$likes' }, reads: { $size: '$reads' }, contentId: 1 }).exec();
-        const topMetric = stats.reduce((acc, { likes, reads, contentId }) => {
-            acc[contentId] = likes + reads;
-            return acc;
-        }, {});
-        res.send({
-            status: 'success',
-            data: topMetric,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
+UserInteractionRouter.get('/content/reads/:contentId', existsContent, readReadsHandler);
+UserInteractionRouter.put('/content/reads/:contentId', existsUser, existsContent, updateReadsHandler);
+UserInteractionRouter.get('/content/likes/:contentId', existsContent, readLikesHandler);
+UserInteractionRouter.put('/content/likes/:contentId', existsUser, existsContent, updateLikesHandler);
+UserInteractionRouter.get('/content/stats', checkTOTP, readStatsHandler);
 
 export default UserInteractionRouter;
