2841d55bb0763829c4ab127e60a57f3c81ecaf27
Aditya DS <adithya18062000@gmail.com>
2022-01-17 19:29:33 +0530

    refactor: move user interaction service business logic to controller

--- routes/UserInteractionRoutes.js
+++ routes/UserInteractionRoutes.js
@@ -1,19 +1,20 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
 import UserInteractionMiddleware from '../middleware/UserInteraction.js';
+import UserInteractionControllers from '../controllers/UserInteractionControllers.js';
 
 dotenv.config({ path: 'routes/.env' });
 
 const UserInteractionRouter = new Router();
+const { existsContent, existsUser, checkTOTP } = UserInteractionMiddleware;
 const {
-    existsContent, existsUser, checkTOTP,
-    readReadsHandler, updateReadsHandler, readLikesHandler, updateLikesHandler, readStatsHandler,
-} = UserInteractionMiddleware;
+    readReads, updateReads, readLikes, updateLikes, readStats,
+} = UserInteractionControllers;
 
-UserInteractionRouter.get('/content/reads/:contentId', existsContent, readReadsHandler);
-UserInteractionRouter.put('/content/reads/:contentId', existsUser, existsContent, updateReadsHandler);
-UserInteractionRouter.get('/content/likes/:contentId', existsContent, readLikesHandler);
-UserInteractionRouter.put('/content/likes/:contentId', existsUser, existsContent, updateLikesHandler);
-UserInteractionRouter.get('/content/stats', checkTOTP, readStatsHandler);
+UserInteractionRouter.get('/content/reads/:contentId', existsContent, readReads);
+UserInteractionRouter.put('/content/reads/:contentId', existsUser, existsContent, updateReads);
+UserInteractionRouter.get('/content/likes/:contentId', existsContent, readLikes);
+UserInteractionRouter.put('/content/likes/:contentId', existsUser, existsContent, updateLikes);
+UserInteractionRouter.get('/content/stats', checkTOTP, readStats);
 
 export default UserInteractionRouter;
