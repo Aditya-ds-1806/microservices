ff92fb41d92318f8aa8e46c734061a74d9a41796
Aditya DS <adithya18062000@gmail.com>
2022-01-17 08:36:45 +0530

    refactor: move user service route handlers to middleware

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,82 +1,15 @@
 import { Router } from 'express';
-import { User } from '../models/User.js';
 import UserMiddleware from '../middleware/User.js';
+
+const {
+    createUserHandler, readUserHandler, existsUser, updateUserHandler, deleteUserHandler,
+} = UserMiddleware;
 
 const UserRouter = new Router();
 
-UserRouter.post('/users', async (req, res) => {
-    try {
-        const {
-            firstName, lastName, emailId, phoneNumber,
-        } = req.body;
-        const newUser = await User.create({
-            firstName,
-            lastName,
-            emailId,
-            phoneNumber,
-        });
-        res.send({
-            status: 'success',
-            data: newUser,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserRouter.get('/users/:userId', UserMiddleware.existsUser, async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const user = await User.findById(userId);
-        res.send({
-            status: 'success',
-            data: user,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserRouter.put('/users/:userId', UserMiddleware.existsUser, async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
-        res.send({
-            status: 'success',
-            data: updatedUser,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
-
-UserRouter.delete('/users/:userId', UserMiddleware.existsUser, async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const deletedUser = await User.findByIdAndDelete(userId);
-        res.send({
-            status: 'success',
-            data: deletedUser,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({
-            status: 'error',
-            message: err.message,
-        });
-    }
-});
+UserRouter.post('/users', createUserHandler);
+UserRouter.get('/users/:userId', existsUser, readUserHandler);
+UserRouter.put('/users/:userId', existsUser, updateUserHandler);
+UserRouter.delete('/users/:userId', existsUser, deleteUserHandler);
 
 export default UserRouter;
